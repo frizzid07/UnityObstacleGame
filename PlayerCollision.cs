583efa8f750cbcd72c6e5b6a93d630a7ec262eda
Tanmay Bhagwat <tanmaybhagwat07@gmail.com>
2018-07-19 18:07:07 +0530

    Add files via upload
    Level 1 of Obstacle Avoidance Game in Unity

---
+++ PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public PlayerMovement movement;
+	//public GameManager gameManager;
+
+	void OnCollisionEnter(Collision collideInfo){
+		if (collideInfo.collider.tag == "Obstacle") {
+			//Debug.Log ("We hit something!");
+			movement.enabled = false;
+			FindObjectOfType<GameManager> ().gameOver ();
+
+		}
+	}
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float forwardForce = 1000f;
+	public float sidewaysForce = 500f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce (0, 0, forwardForce * Time.deltaTime);
+
+		if (Input.GetKey ("d")) {
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey ("a")) {
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (rb.position.y < -4f) {
+			FindObjectOfType<GameManager> ().gameOver ();
+		}
+	}
+}
